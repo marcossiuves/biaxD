0775617eff15f1d891c005c2fd7c565f85818061
marcossiuves <marcossiuves@gmail.com>
2021-05-15 18:48:44 -0300

    Add files via upload

--- CommandList.java
+++ CommandList.java
@@ -3,11 +3,10 @@ package test.chatbot;
 public class CommandList {
 
 	String response;
-	
+
 	public CommandList() {
 
 	}
-
 
 	public String justChatting(int messageType) {
 
@@ -15,40 +14,42 @@ public class CommandList {
 
 		String response = r.allResponses[messageType + 1];
 		this.response = response;
-		
+
 		return this.response;
 	}
-	
+
 	public int typeResponse(String message) {
-		
+
 		responses r = new responses();
-		
+
 		String[] anws = r.allResponses;
-		
-		
+
 		int position = 0;
-		
-		for(int count = 0; count <= anws.length; count++) {
-			
-			if(message.equalsIgnoreCase(anws[count])) {
+
+		for (int count = 0; count <= anws.length; count++) {
+
+			try {
+				
+			if (message.equalsIgnoreCase(anws[count])) {
 				position = count;
 				break;
 			}
-			
+
+			} catch (Exception e) {
+				System.out.println("Mensagem não cadastrada");
+				System.exit(0);
+			}
+
 		}
 
 		return position;
 	}
 
-	public String response(int moodValue, int typeResponse) {
+	public String response(int typeResponse) {
 
 		responses anws = new responses();
 
-		if (moodValue == 0) {
-
-			this.response = anws.allResponses[typeResponse + 1];
-
-		}
+		this.response = anws.allResponses[typeResponse + 1];
 
 		return this.response;
 	}
--- Main.java
+++ Main.java
@@ -4,21 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		
-		String message;
+
+		String message = "";
 		CommandList command = new CommandList();
-		
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Inicio: ");
-		message = input.nextLine();
-		
-		int position = command.typeResponse(message);
-		System.out.println(command.justChatting(position));
-		
-		
+		while (!message.equalsIgnoreCase("sair")) {
+			message = input.nextLine();
+			int position = command.typeResponse(message);
+			System.out.println(command.justChatting(position));
+		}
+
 		input.close();
-		
-		
+
 	}
 }
